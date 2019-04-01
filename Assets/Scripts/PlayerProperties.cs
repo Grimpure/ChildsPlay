71cf234737774fbf01cd5f3edff50b49fbc44ad1
Mathijs van Dinther <ma.van.dinther@sintlucasedu.nl>
2019-04-01 22:42:04 +0200

    Made seperate script for properties.
    Yet to be implemented properly through crocodile script

---
+++ Assets/Scripts/PlayerProperties.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProperties : MonoBehaviour
+{
+    public enum Color { Red, Blue, Yellow, Purple }
+
+    public bool isRed;
+    public bool isBlue;
+    public bool isYellow;
+    public bool isPurple;
+
+    public float score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
