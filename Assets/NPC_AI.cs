cb406ef20e93445864f4b542141c91b90d045dee
Baris Buba <Skullyurdb@gmail.com>
2019-04-03 23:28:55 +0200

    NPC behaveviour
    NPC can take random paths
    NPC have different behaviors: Idle & crossing
    different behavior can be activated at certain events
    NPC behavior can be checked if it used and activated correctly
    NPC knows how to play by the rules
    NPC knows where they can and can't stand
    NPC knows what to target and what not

---
+++ Assets/NPC_AI.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPC_AI : MonoBehaviour
+{
+    public Vector3 dest_cord;
+
+    public Transform dest;
+
+    public int currentLine = 0;
+
+    public bool stayOnPlace = true;
+
+    public Transform[] startDestination;
+    public Transform[] Destination1;
+    public Transform[] Destination2;
+    public Transform[] Destination3;
+    public Transform[] endDestination;
+
+
+
+    NavMeshAgent thisNPC;
+
+    public enum colours {Purple, Yellow, Blue, Red }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        colours npcColour = (colours)colours.ToObject(typeof(colours), Random.Range(0, 5));
+        thisNPC = this.GetComponent<NavMeshAgent>();
+        Debug.Log(npcColour); // REMOVE WHEN DONE
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dest_cord = dest.position;
+        thisNPC.SetDestination(dest_cord);
+
+        switch (stayOnPlace)
+        {
+            case false:
+                goForward();
+                break;
+            case true:
+                //No action required
+                break;
+        }
+
+    }
+    void goForward()
+    {
+        currentLine++;
+
+        switch (currentLine)
+        {
+            case 0: //Begin Destination
+                // Not sure if this will be used
+                break;
+            case 1:
+                dest = Destination1[Random.Range(0,5)];
+                break;
+            case 2:
+                dest = Destination2[Random.Range(0, 5)];
+                break;
+            case 3:
+                dest = Destination3[Random.Range(0, 5)];
+                break;
+            case 4: //End Destination
+                dest = endDestination[Random.Range(0, 5)];
+                break;
+        }
+
+
+        stayOnPlace = true; // automatically resets the bool so the action won't repeat itself untill it is supposed to.
+    }
+}
